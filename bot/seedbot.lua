51ffe86cd3a216a8efa97c4ab1ba0decc06e7575
jeiboor <antispam0099@Gmail.com>
2016-03-20 02:11:18 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {160890821(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -322,6 +322,7 @@ This command will send text to [group_id]
 
 » Only owner can use res,setowner,promote,demote and log commands
 
+سازنده : @MR_saf
 ]],
     help_text = [[
 Commands list :
@@ -426,6 +427,8 @@ Will return group ban list
 » Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands
 
 » Only owner can use res,setowner,promote,demote and log commands
+
+سازنده : @MR_saf
 
 ]]
   }
